35d7afe99e9875680b1239a4c922d25e4113dc15
NavaneethMohan9 <nmyeturi@gmail.com>
2025-03-21 13:02:17 +0530

    misc changes

--- js/app.js
+++ js/app.js
@@ -7,38 +7,47 @@ document.getElementById("getWeather").addEventListener("click", function(event) 
 		displayError("Please enter a city name.");
 		return;
 	}
-
-	const apiKey = TOKEN;
 	const url2 = `https://api.weatherapi.com/v1/current.json?`;
-	const url = `https://wttr.in/${city}?format=j1`
+	const url = `https://wttr.in/${city}?format=v2`
+	let img = ""
 	const options = {
 		method: 'GET'
 	}
+	fetch(url)
+		.then(response => response.text())
+		.then(html => {
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, 'text/html');
+			const images = doc.querySelector('img');
+			img = 'https://wttr.in/' + images.src.split("/")[3];
+			console.log(img);
+		})
+		.catch(error => console.error('Error fetching HTML:', error));
 	fetch(url2 + new URLSearchParams({
-		key: apiKey,
+		key: TOKEN,
 		q: city,
 		aqi: 'yes'
 	}).toString())
-	.then(function(response) {
-		return response.json().then(data => {
-			if(!response.ok) {
-				displayError(data.error.message);
-				console.log(data.error.message);
-				throw new Error(data.message);
-			}
-			return data;
+		.then(function(response) {
+			return response.json().then(data => {
+				if(!response.ok) {
+					displayError(data.error.message);
+					console.log(data.error.message);
+					throw new Error(data.message);
+				}
+				return data;
+			})
 		})
-	})
-	.then(data => {
-		displayWeather(data);
-	})
-	.catch(error => {
-		// displayError("Error fetching the weather data.");
-		console.log(error);
-	})
+		.then(data => {
+			displayWeather(data, img);
+		})
+		.catch(error => {
+			// displayError("Error fetching the weather data.");
+			console.log(error);
+		})
 });
 
-function displayWeather(data) {
+function displayWeather(data, img) {
 	// Hide the error message, if any
 	document.getElementById("errorMessage").textContent = "";
 
@@ -46,7 +55,7 @@ function displayWeather(data) {
 	document.getElementById("weather-details").style.display = "block";
 	document.getElementById("weatherTitle").textContent = `Current weather for ${data.location.name}, ${data.location.region}, ${data.location.country}: `;
 	document.getElementById("conditionText").textContent = `Condition: ${data.current.condition.text}`;
-	// document.getElementById("conditionImage").src = `https://${data.current.condition.icon.substring(2)}`;
+	document.getElementById("conditionImage").src = img;
 	document.getElementById("temperature").innerHTML = `Temperature: ${data.current.temp_c}°C. Feels like ${data.current.feelslike_c}°C`;
 	document.getElementById("aqi").innerHTML = `US EPA index: ${data.current.air_quality['us-epa-index']}`;
 	document.getElementById("humidity").innerHTML = `Humidity: ${data.current.humidity}%`;
